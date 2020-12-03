faad9af6e208f9db58f3432bfc4eb3a3db189f2a
韩文辉 <hanwh@dehuinet.com>
2020-12-03 22:51:30 +0800

    update

--- src/components/zlgl/index.vue
+++ src/components/zlgl/index.vue
@@ -4,18 +4,27 @@
       :visible.sync="visible"
       :close-on-press-escape="false"
       :show-close="false"
-      width="240px"
+      width="60vw"
       title="资料管理"
       @close="$emit('update:zlglShow',false)"
       center
     >
-      <el-tree :data="folders" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
-      <div>
-        <el-table :data="tableData" style="width: 100%">
-          <el-table-column prop="no" label="文件编号" width="180"></el-table-column>
-          <el-table-column prop="name" label="文件名" width="180"></el-table-column>
-          <el-table-column prop="type" label="文件类型"></el-table-column>
-        </el-table>
+      <div class="box">
+        <el-tree class="tree" :data="folders" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+        <div class="datalist">
+          <el-table :data="tableData">
+            <el-table-column prop="no" label="文件编号"></el-table-column>
+            <el-table-column prop="name" label="文件名"></el-table-column>
+            <el-table-column prop="type" label="文件类型"></el-table-column>
+          </el-table>
+          <el-pagination
+          class="margin-top-l"
+            layout="prev, pager, next"
+            :total="currentTotal"
+            small
+            @current-change="pageChange"
+          ></el-pagination>
+        </div>
       </div>
     </el-dialog>
   </div>
@@ -41,7 +50,8 @@ export default {
         children: "children",
         label: "desc"
       },
-      currentNo: 0
+      currentNo: 0,
+      currentTotal: 10
     };
   },
   computed: {
@@ -64,17 +74,39 @@ export default {
     async getFolders() {
       this.folders = await GetFolders({ ProjectNo: this.projectNo });
     },
-    async handleNodeClick(e) {
-      console.log(e.no);
+    async getList(currentPage = 1) {
+      const {list,total} = await GetDocsByFolderId({
+        folderid: this.currentNo,
+        CurrentPage: currentPage,
+        PageSize: 10
+      });
+      this.tableData = list;
+      this.currentTotal = total;
+    },
+    handleNodeClick(e) {
       this.currentNo = e.no;
-      this.tableData = await GetDocsByFolderId({ folderid: e.no });
+      this.getList(1);
+    },
+    pageChange(e) {
+      this.getList(e);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.box{
-  
+.box {
+  display: flex;
+  width: 100%;
+  min-height: 600px;
+  justify-content: space-between;
+  .tree {
+    width: 380px;
+    height: 550px;
+    overflow: auto;
+  }
+  .datalist {
+    width: 650px;
+  }
 }
 </style>
