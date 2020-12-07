c5dcd5ed7b2ff178a2381b26ddaf17692b8acbdd
hubert <zixinweb@163.com>
2020-12-07 09:46:14 +0800

    update webpack

--- vue.config.js
+++ vue.config.js
@@ -55,26 +55,11 @@ module.exports = {
             }, ])
             // set alias for src
         config.resolve.alias.set('@', resolve('src'))
-
-        // when there are many pages, it will cause too many meaningless requests
+        config.resolve.modules.set('node_modules', resolve('node_modules'))
+            // when there are many pages, it will cause too many meaningless requests
         config.plugins.delete('prefetch')
 
-        // set svg-sprite-loader
-        // config.module
-        //   .rule('svg')
-        //   .exclude.add(resolve('src/icons'))
-        //   .end()
-        // config.module
-        //   .rule('icons')
-        //   .test(/\.svg$/)
-        //   .include.add(resolve('src/icons'))
-        //   .end()
-        //   .use('svg-sprite-loader')
-        //   .loader('svg-sprite-loader')
-        //   .options({
-        //     symbolId: 'icon-[name]',
-        //   })
-        //   .end()
+
 
         config.when(process.env.NODE_ENV !== 'development', (config) => {
             config
