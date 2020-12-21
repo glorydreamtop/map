669b7895eafee337899ad947274cd537ca8f6aeb
zhuyajing <1352717859@qq.com>
2020-12-21 08:54:42 +0800

    提交

---
+++ src/components/zxxm/dianliPage/dianli_table.vue
@@ -0,0 +1,178 @@
+<template>
+	<div class="tableMain">
+		<div class="header_table_title">
+			<el-button icon="el-icon-plus" @click="addClick()" plain> </el-button>
+		</div>
+		<div class="body_table_mian">
+			<el-table v-loading="tableLoad"  :data="tableData" row-key="id" :tree-props="{children: 'children'}" border style="width: 100%" height="50vh">
+				<el-table-column prop="SerialNumber" label="编号" width="150" align="center"> </el-table-column>
+				<el-table-column prop="Createdate" label="申请时间" align="center"> </el-table-column>
+				<el-table-column prop="CountyDESC" label="区县" width="100" align="center"> </el-table-column>
+				<el-table-column  label="高程范围" align="center"> 
+				   <el-table-column prop="CountyDESC" label="范围1" width="100" align="center"> </el-table-column>
+				   <el-table-column prop="CountyDESC" label="范围2" width="100" align="center"> </el-table-column>
+				</el-table-column>
+				<el-table-column prop="Region" label="地区类型" align="center"> </el-table-column>
+				<el-table-column prop="" label="线路名称" width="100" align="center"> </el-table-column>
+				<el-table-column prop="TownDESC" label="起讫点" width="100" align="center"> </el-table-column>
+				<el-table-column prop="VillageDESC" label="权属部门" align="center"> </el-table-column>
+				<el-table-column prop="VillageGroupDESC" label="主管机构"  align="center"> </el-table-column>
+				<el-table-column prop="VillageGroupDESC" label="等级"  align="center"> </el-table-column>
+				<el-table-column fixed="right" label="操作" width="300" align="center">
+					<template slot-scope="scope">
+						<el-button title="查看" icon="el-icon-view" type="primary" plain @click="lookClick(scope.row)"></el-button>
+						<el-button title="修改" icon="el-icon-edit" type="warning" plain @click="editClick(scope.row)"></el-button>
+						<el-button title="删除" icon="el-icon-delete" type="danger" plain @click="delClick(scope.row)"></el-button>
+
+					</template>
+				</el-table-column>
+			</el-table>
+			<el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="formeData.CurrentPage"
+				  :page-size="formeData.PageSize"
+				  :total="total">
+		    </el-pagination>
+		</div>
+		<div>
+			<el-dialog :title="dialogTitle"  :append-to-body="true" @close='closeDialog' :visible.sync="showFlag" v-model="showFlag"
+			 class="newStyleDialog " custom-class="jbqk_add_table1_dialog">
+				<dianliHeaderAdd :dialog-type="dialogType" v-on:showStudes="showStudescode" :dialog-form="dialogForm" v-if="showFlag" ></dianliHeaderAdd>
+			</el-dialog>
+		</div>
+	</div>
+</template>
+
+<script>
+	import {
+		mapGetters
+	} from 'vuex'
+	import {
+		GetAllBaseTablesBaseAttrs,DeleteBaseTable
+	} from '@/api'
+	import dianliHeaderAdd from './dianli_header_add' 
+	export default {
+		name: "dianliTable",
+		props: {},
+		components: {
+			dianliHeaderAdd,
+		},
+		computed: {
+			...mapGetters(['projectNo','BaseType'])
+		},
+		data() {
+			return {
+				tableData: [],
+				activeName: "",
+				dialogTitle: '', //弹出框标题
+				dialogForm: "", //弹出框表单
+				showFlag: false, //弹出框显隐状态
+				dialogType: '', //弹出框操作类型
+				total: 0,
+				formeData: {
+					CurrentPage: 1,
+					PageSize: 6,
+				},
+				tableLoad:false,
+
+
+			};
+		},
+		created() {},
+		mounted() {
+			if (this.projectNo) {
+				this.tableInit(); //表格初始化
+			}
+
+		},
+		methods: {
+			tableInit() {
+				var data = {
+					BaseType: this.BaseType,
+					ProjectNo: this.projectNo,
+					...this.formeData
+				};
+				this.tableLoad=true;
+				GetAllBaseTablesBaseAttrs(data).then((res) => {
+					console.log(res)
+					this.tableLoad=false;
+					this.tableData = res.list;
+					this.total = res.total;
+				})
+				.catch((error) => {
+					this.tableData = [];
+					this.tableLoad=false;
+					console.log(error)
+				})
+			},
+
+			handleSizeChange(val) { //每页#条
+				this.formeData.PageSize = val;
+				this.tableInit();
+			},
+			handleCurrentChange(val) { //当前第几页
+				this.formeData.CurrentPage = val;
+				this.tableInit();
+			},
+			lookClick(row) { //查看
+				this.dialogForm = row;
+				this.dialogTitle = '查看电力调查表';
+				this.dialogType = 'look';
+				this.showFlag = true;
+
+			},
+			delClick(row) {
+				var self=this;
+				this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+					confirmButtonText: '确定',
+					cancelButtonText: '取消',
+					center: true,
+					type: 'warning'
+				}).then(() => {
+					DeleteBaseTable({id:row.KeyNo}).then((res) => {
+						console.log(res)
+							self.$message({
+								message: '操作成功',
+								type: 'success',
+								center: true
+							});
+
+							self.tableInit();
+					
+					}).catch((res) => {
+						console.log(res);
+					})
+
+
+				}).catch((res) => {
+					
+				})
+			},
+			editClick(row) { //修改
+				this.dialogForm = row;
+				this.dialogTitle = '编辑电力调查表';
+				this.dialogType = 'edit';
+				this.showFlag = true;
+
+			},
+			addClick() { //添加
+				this.dialogForm = '';
+				this.dialogTitle = '添加电力调查表';
+				this.dialogType = 'add';
+				this.showFlag = true;
+
+			},
+			closeDialog() { //关闭弹出框
+				this.tableInit();
+			},
+			showStudescode(data) { //监听弹出框是关还是闭
+
+			},
+		}
+	};
+</script>
+
+
+<style scoped="scoped">
+	.body_table_mian {
+		margin-top: 1vh;
+	}
+</style>
--- src/components/zxxm/index.vue
+++ src/components/zxxm/index.vue
@@ -1,20 +1,132 @@
 <template>
-  <div></div>
+	<div>
+		<el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="6" v-for="item in titleData">
+			<div class="leftMain">
+				<div class="item flex-col align-center" @click="openDetail(item)">
+					<i class="al-icon-xiangmuguanli text-light"></i>
+					<span>{{item.name}}</span>
+				</div>
+			</div>
+
+		</el-col>
+		<el-dialog
+		  :title="dialogTitle"
+		  :append-to-body="true"
+		  @close="closeDialog"
+		  :visible.sync="showFlag"
+		  v-model="showFlag"
+		  class="newStyleDialog"
+		  custom-class="jbqkTable_dialog"
+		>
+		  <!-- 动态组件 -->
+		  <component
+		    :is="dialogIndex"
+		    :dialog-type="dialogType"
+		    v-on:showStudes="showStudescode"
+		    :dialog-form="dialogForm"
+		    v-if="showFlag"
+		  ></component>
+		</el-dialog>
+	</div>
 </template>
 
 <script>
-export default {
-  name: 'Zxxm',
-  props: {},
-  data() {
-    return {}
-  },
-  created() {},
-  mounted() {},
-  methods: {}
-}
+	import dianliTable from "./dianliPage/dianli_table.vue"; //电力
+	import dianxinTable from "./dianxinPage/dianxin_table.vue"; //电器
+	import gongluTable from "./gongluPage/gonglu_table.vue"; //公路
+	import guangboTable from "./guangboPage/guangbo_table.vue"; //广播电视
+	import wenwuTable from "./wenwuPage/wenwu_table.vue"; //文物
+	import shuidianTable from "./shuidianPage/shuidian_table.vue"; //水电
+	import shuiyunTable from "./shuiyunPage/shuiyun_table.vue"; //水利水运
+	import tieluTable from "./tieluPage/tielu_table.vue"; //铁路
+	import zongjiaoTable from "./zongjiaoPage/zongjiao_table.vue"; //宗教
+	export default {
+		name: 'Zxxm',
+		props: {},
+		components:{dianliTable,dianxinTable,gongluTable,guangboTable,wenwuTable,shuidianTable,shuiyunTable,tieluTable,zongjiaoTable},
+		data() {
+			return {
+				dialogTitle: "", //弹出框标题
+				dialogForm: "", //弹出框表单
+				showFlag: false, //弹出框显隐状态
+				dialogType: '', //弹出框操作类型
+				dialogIndex:'',//组件显示索引
+				titleData: [{
+						name: '电力',
+						index: 'dianliTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '电信',
+						index: 'dianxinTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '铁路',
+						index: 'tieluTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '公路',
+						index: 'gongluTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '水运',
+						index: 'shuiyunTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '水利水电',
+						index: 'shuidianTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '文物',
+						index: 'wenwuTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '广播电视',
+						index: 'guangboTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+					{
+						name: '农村文教',
+						index: 'zongjiaoTable',
+						icon: 'al-icon-xiangmuguanli'
+					},
+				],
+			}
+		},
+		created() {},
+		mounted() {},
+		methods: {
+			openDetail(data){
+				this.dialogTitle = `${data.name}调查表 `;
+				this.showFlag = true;
+				this.dialogForm = "";
+				this.dialogType = 'table';
+				this.dialogIndex=data.index;
+			},
+			closeDialog() {
+			  //关闭弹出框
+			},
+			showStudescode(data) {
+			  //监听弹出框是关还是闭
+			},
+		}
+	}
 </script>
 
 <style lang="scss" scoped>
-
+	.item {
+		[class*="icon"] {
+			font-size: 36px;
+			cursor: pointer;
+		}
+	}
+	.leftMain{
+		// padding: 1vh 0;
+	}
 </style>
